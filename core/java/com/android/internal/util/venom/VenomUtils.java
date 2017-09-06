ebb49cf011f246dda93b40fa80a625cce6e12470
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    core: Add api to take screenshots
    Omni: extracted from DUI
    add a public call to XtendedUtils
    
    Change-Id: If71944466c703a9b3fe54aea6bb21150b0a082bb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/venom/VenomUtils.java
+++ core/java/com/android/internal/util/venom/VenomUtils.java
@@ -48,12 +48,6 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.net.ConnectivityManager;
 
-import android.os.Handler;
-import android.os.UserHandle;
-import android.provider.Settings;
-import android.os.SystemProperties;
-
-import java.util.List;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -64,6 +58,9 @@ public class VenomUtils {
 
     public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
     public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -162,21 +159,5 @@ public class VenomUtils {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
-    }
-
-    public static ActivityInfo getRunningActivityInfo(Context context) {
-        final ActivityManager am = (ActivityManager) context
-                .getSystemService(Context.ACTIVITY_SERVICE);
-        final PackageManager pm = context.getPackageManager();
-
-        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
-        if (tasks != null && !tasks.isEmpty()) {
-            ActivityManager.RunningTaskInfo top = tasks.get(0);
-            try {
-                return pm.getActivityInfo(top.topActivity, 0);
-            } catch (PackageManager.NameNotFoundException e) {
-            }
-        }
-        return null;
     }
 }
