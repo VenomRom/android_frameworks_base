246de3cffd0c7522a8dacdfa9f5be8c3440d2b78
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-22 19:01:07 -0300

    StatusBarSignalPolicy: Fix NPE due do ext telephony
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: FATAL EXCEPTION: main
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: Process: com.android.systemui, PID: 13539
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: java.lang.RuntimeException: Unable to create service com.android.systemui.SystemUIService: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean com.android.internal.telephony.util.TelephonyExtUtils.hasService()' on a null object reference
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.app.ActivityThread.handleCreateService(ActivityThread.java:3544)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.app.ActivityThread.access$1300(ActivityThread.java:199)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1666)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:193)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:6669)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:495)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:858)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean com.android.internal.telephony.util.TelephonyExtUtils.hasService()' on a null object reference
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.StatusBarSignalPolicy$MobileIconState.<init>(StatusBarSignalPolicy.java:425)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.StatusBarSignalPolicy$MobileIconState.<init>(StatusBarSignalPolicy.java:413)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.StatusBarSignalPolicy.setSubs(StatusBarSignalPolicy.java:275)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.statusbar.policy.NetworkControllerImpl.addCallback(NetworkControllerImpl.java:400)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.StatusBarSignalPolicy.<init>(StatusBarSignalPolicy.java:96)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.StatusBar.start(StatusBar.java:780)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.SystemBars.createStatusBarFromConfig(SystemBars.java:71)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.SystemBars.start(SystemBars.java:42)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.SystemUIApplication.startServicesIfNeeded(SystemUIApplication.java:185)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.SystemUIApplication.startServicesIfNeeded(SystemUIApplication.java:129)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at com.android.systemui.SystemUIService.onCreate(SystemUIService.java:39)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	at android.app.ActivityThread.handleCreateService(ActivityThread.java:3532)
    10-22 18:52:45.834 13539 13539 E AndroidRuntime: 	... 8 more
    
    Change-Id: Id7d3d9fd49b8578e9dcaa5b6c2ee7059f36c3fa7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui.statusbar.phone;
 
+import android.app.ActivityThread;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Rect;
@@ -78,8 +79,6 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
     private ArrayList<MobileIconState> mMobileStates = new ArrayList<MobileIconState>();
     private WifiIconState mWifiIconState = new WifiIconState();
 
-    private static TelephonyExtUtils extTelephony;
-
     public StatusBarSignalPolicy(Context context, StatusBarIconController iconController) {
         mContext = context;
 
@@ -99,7 +98,6 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         mSecurityController.addCallback(this);
 
         TelephonyExtUtils.getInstance(context).addListener(this);
-        extTelephony = TelephonyExtUtils.getInstance(context);
     }
 
     @Override
@@ -426,6 +424,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         private MobileIconState(int subId) {
             super();
             this.subId = subId;
+            TelephonyExtUtils extTelephony = TelephonyExtUtils.getInstance(ActivityThread.currentActivityThread().getSystemContext());
             if (extTelephony.hasService()) {
                 mProvisioned = extTelephony.isSubProvisioned(subId);
             }
